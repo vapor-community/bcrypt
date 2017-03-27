ec439e33f197a5c589715e2ccb069a6fd50fa305
Logan Wright <logan.william.wright@gmail.com>
2017-03-27 14:42:11 +0100

    beta dependencies

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     name: "BCrypt",
     dependencies: [
         // Module for generating random bytes and numbers.
-        .Package(url: "https://github.com/vapor/random.git", majorVersion: 0)
+        .Package(url: "https://github.com/vapor/random.git", Version(1,0,0, prereleaseIdentifiers: ["beta"]))
     ]
 )
